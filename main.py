e7a260fc000945f58b31b572ebd46ee0bf0e9aac
Edward O'Connor <hober0@gmail.com>
2008-05-14 12:20:45 -0700

    Fix missing log_exception error.

--- main.py
+++ main.py
@@ -13,10 +13,6 @@ import django.core.signals
 import django.db
 import django.dispatch.dispatcher
 
-# Log errors.
-django.dispatch.dispatcher.connect(
-   log_exception, django.core.signals.got_request_exception)
-
 # Unregister the rollback event handler.
 django.dispatch.dispatcher.disconnect(
     django.db._rollback_on_exception,
